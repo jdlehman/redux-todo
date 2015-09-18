0a898a81a824560d05d692686206a212cf09dd70
Jonathan Lehman <jonathan.lehman91@gmail.com>
2015-09-18 15:07:51 -0400

    Handle todo actions in todos reducer

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,3 +1,4 @@
+import * as TodoTypes from 'types/TodoTypes';
 import uuid from 'node-uuid';
 import Immutable from 'immutable';
 
@@ -8,6 +9,40 @@ function initialState() {
   ]);
 }
 
+function createItem(text) {
+  return Immutable.fromJS({id: uuid.v4(), text});
+}
+
+function findTodoIndex(state, id) {
+  return state.findIndex(todo => todo.get('id') === id);
+}
+
+function updateTodoById(state, id, calcNewValue) {
+  return state.updateIn(
+    [
+      findTodoIndex(state, id)
+    ],
+    todo => calcNewValue(todo)
+  );
+}
+
 export default function todos(state = initialState(), action) {
-  return state;
+  switch (action.type) {
+    case TodoTypes.CHECK:
+      return updateTodoById(
+        state,
+        action.payload.id,
+        view => view.set('done', true)
+      );
+    case TodoTypes.UNCHECK:
+      return updateTodoById(
+        state,
+        action.payload.id,
+        view => view.set('done', false)
+      );
+    case TodoTypes.ADD:
+      return state.push(createItem(action.payload.text));
+    default:
+      return state;
+  }
 }
